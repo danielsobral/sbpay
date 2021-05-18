a0df3a83d893a5389ff14a6f4d322a6f8a517ef1
Daniel <danielsobralnascimento@hotmail.com>
2021-05-18 17:56:51 -0300

    Web Service de pagamento

---
+++ src/main/java/com/daniel/sbpay/PaymentResponse.java
@@ -0,0 +1,23 @@
+package com.daniel.sbpay;
+
+public class PaymentResponse {
+
+	private StatusPagamento status;
+	private String error;
+	
+	public PaymentResponse(StatusPagamento status) {
+		this.status = status;
+	}
+	
+	public PaymentResponse(String error) {
+		this.error = error;
+	}
+	
+	public String getError() {
+		return error;
+	}
+	
+	public StatusPagamento getStatus() {
+		return status;
+	}
+}
